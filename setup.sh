56d90c81500e6f72420e02d7e0d4b022caf919a4
Taha Farahani <tahacodes@proton.me>
2024-04-04 01:45:46 +0330

    Simple setup sctipt

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 # zsh
 if cp zsh/.zshrc $HOME/.zshrc; then
-  echo "Copied zsh config"
+    echo "Copied zsh configs"
 fi
 
 # tmux
 if cp tmux/.tmux.conf $HOME/.tmux.conf; then
-  echo "Copied tmux config"
+    echo "Copied tmux configs"
 fi
 
 # nvim
 if cp -r nvim/* $HOME/.config/nvim/.; then
-  echo "Copied nvim config"
+    echo "Copied nvim configs"
 fi
 
 # kitty
 if cp kitty/kitty.conf $HOME/.config/kitty/kitty.conf; then
-  echo "Copied kitty config"
+    echo "Copied kitty configs"
 fi
